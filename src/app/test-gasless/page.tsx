4640ce64e7b9d577435fcb70256a74533aa1789f
Filip <82238474+jumperdevweb3@users.noreply.github.com>
2024-04-08 12:47:09 +0200

    move gasless logic to separated component

--- src/app/test-gasless/page.tsx
+++ src/app/test-gasless/page.tsx
@@ -1,45 +1,7 @@
 "use client";
-import { Flex, Button } from "@chakra-ui/react";
-import { useAddress } from "@thirdweb-dev/react";
+import { Flex } from "@chakra-ui/react";
 import { TestGasless } from "@/components/test/TestGasless";
-import { useUser } from "@/hooks/useUser";
 export default function TestPage() {
-  const data = useUser();
-  const sellerWalletAddr = useAddress();
-  console.log(data);
-  const createEventHandler = async () => {
-    const res = await fetch("/api/events", {
-      method: "POST",
-      body: JSON.stringify({
-        title: "test",
-        sellerEmail: "filip@licenserocks.de",
-        sellerWalletAddr,
-        description: "test",
-        coverUrl: "",
-        startsAt: new Date(1712572158),
-        finishAt: new Date(1712575758),
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    console.log(res);
-  };
-
-  const deployHandler = async () => {
-    const id = "cluqr4tnw00022vgbdf1qpmx4";
-
-    const res = await fetch(`/api/events/${id}/deployContract`, {
-      method: "POST",
-      body: JSON.stringify({
-        contract: "LotteryV1",
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    console.log(res);
-  };
   return (
     <Flex
       my={10}
@@ -49,9 +11,6 @@ export default function TestPage() {
       gap={2}
     >
       <TestGasless />
-
-      <Button onClick={createEventHandler}>Create event</Button>
-      <Button onClick={deployHandler}>Deploy lottery</Button>
     </Flex>
   );
 }
---
+++ src/components/test/TestGasless.tsx
@@ -0,0 +1,787 @@
+"use client";
+import { Flex, Button, Link } from "@chakra-ui/react";
+import { GelatoRelay, CallWithERC2771Request } from "@gelatonetwork/relay-sdk";
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import { useAddress, useChainId, useSigner } from "@thirdweb-dev/react";
+
+const apiKey = process.env.GELATO_API_KEY as string;
+const target = "0xafc3dc42d4b172564c397229996bb968e426b039";
+
+export const TestGasless = () => {
+  const [initiated, setInitiated] = useState(false);
+  const [taskId, setTaskId] = useState("");
+  const [taskStatus, setTaskStatus] = useState("N/A");
+  const [txHash, setTxHash] = useState("");
+
+  const address = useAddress();
+  const chainId = useChainId() as any;
+  const signer = useSigner() as any;
+
+  const sendRelayRequest = async () => {
+    setInitiated(true);
+    setTaskId("");
+    setTxHash("");
+    setTaskStatus("Loading...");
+
+    const relay = new GelatoRelay();
+    const contract = new ethers.Contract(target, abi, signer);
+
+    const { data } = await contract.populateTransaction.approve(
+      "0x727b6D0a1DD1cA8f3132B6Bc8E1Cfa0C04CAb806",
+      1000,
+    );
+
+    if (!chainId || !data || !address) return;
+
+    const request: CallWithERC2771Request = {
+      chainId,
+      target,
+      data,
+      user: address,
+    };
+
+    const relayResponse = await relay.sponsoredCallERC2771(
+      request,
+      signer,
+      apiKey,
+    );
+    setTaskId(relayResponse.taskId);
+  };
+
+  useEffect(() => {
+    let statusQuery: NodeJS.Timer | any;
+    if (taskId === "") return;
+
+    const getTaskState = async (getTxHash = false) => {
+      try {
+        const url = `https://relay.gelato.digital/tasks/status/${taskId}`;
+        const response = await fetch(url);
+        const responseJson = await response.json();
+        setTaskStatus(responseJson.task.taskState);
+        if (getTxHash) {
+          setTxHash(responseJson.task.transactionHash);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (taskStatus !== "ExecSuccess") {
+      statusQuery = setInterval(() => {
+        getTaskState();
+      }, 1500);
+    } else {
+      getTaskState(true);
+      setInitiated(false);
+    }
+
+    return () => {
+      clearInterval(statusQuery);
+    };
+  }, [taskId, taskStatus]);
+  return (
+    <Flex
+      my={10}
+      justifyContent={"center"}
+      flexDirection={"column"}
+      alignItems={"center"}
+      gap={2}
+    >
+      <p>
+        {" "}
+        <b>
+          {" "}
+          {address && chainId === 123420111
+            ? ""
+            : "Connect your wallet to Polygon to begin"}{" "}
+        </b>{" "}
+      </p>
+      <Button
+        isDisabled={!(address && chainId === 123420111)}
+        onClick={sendRelayRequest}
+      >
+        {initiated && taskStatus !== "ExecSuccess"
+          ? "Gelato go brr"
+          : "Trigger gasless tx"}
+      </Button>
+
+      <div>
+        <p>
+          <b>Task ID:</b>{" "}
+          <Link
+            href={`https://relay.gelato.digital/tasks/status/${taskId}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            color={"blue.400"}
+          >
+            {" "}
+            {taskId !== "" ? taskId : "Waiting for Relay Request"}{" "}
+          </Link>
+        </p>
+        <p>
+          <b>Status:</b> {taskStatus || "-"}
+        </p>
+
+        {txHash !== "" ? (
+          <p>
+            <b>Tx Hash: </b>
+            <Link
+              href={`https://opcelestia-raspberry.gelatoscout.com/tx/${txHash}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              color={"blue.400"}
+            >
+              {txHash}
+            </Link>
+          </p>
+        ) : (
+          ""
+        )}
+      </div>
+    </Flex>
+  );
+};
+
+const abi: any = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "initialOwner",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "ECDSAInvalidSignature",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "length",
+        type: "uint256",
+      },
+    ],
+    name: "ECDSAInvalidSignatureLength",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "s",
+        type: "bytes32",
+      },
+    ],
+    name: "ECDSAInvalidSignatureS",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "allowance",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "needed",
+        type: "uint256",
+      },
+    ],
+    name: "ERC20InsufficientAllowance",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "sender",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "balance",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "needed",
+        type: "uint256",
+      },
+    ],
+    name: "ERC20InsufficientBalance",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "approver",
+        type: "address",
+      },
+    ],
+    name: "ERC20InvalidApprover",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "receiver",
+        type: "address",
+      },
+    ],
+    name: "ERC20InvalidReceiver",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "sender",
+        type: "address",
+      },
+    ],
+    name: "ERC20InvalidSender",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+    ],
+    name: "ERC20InvalidSpender",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "deadline",
+        type: "uint256",
+      },
+    ],
+    name: "ERC2612ExpiredSignature",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "signer",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+    ],
+    name: "ERC2612InvalidSigner",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "currentNonce",
+        type: "uint256",
+      },
+    ],
+    name: "InvalidAccountNonce",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "InvalidShortString",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+    ],
+    name: "OwnableInvalidOwner",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+    ],
+    name: "OwnableUnauthorizedAccount",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "str",
+        type: "string",
+      },
+    ],
+    name: "StringTooLong",
+    type: "error",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "Approval",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [],
+    name: "EIP712DomainChanged",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "previousOwner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "OwnershipTransferred",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "Transfer",
+    type: "event",
+  },
+  {
+    inputs: [],
+    name: "DOMAIN_SEPARATOR",
+    outputs: [
+      {
+        internalType: "bytes32",
+        name: "",
+        type: "bytes32",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+    ],
+    name: "allowance",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "approve",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+    ],
+    name: "balanceOf",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "decimals",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "eip712Domain",
+    outputs: [
+      {
+        internalType: "bytes1",
+        name: "fields",
+        type: "bytes1",
+      },
+      {
+        internalType: "string",
+        name: "name",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "version",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "chainId",
+        type: "uint256",
+      },
+      {
+        internalType: "address",
+        name: "verifyingContract",
+        type: "address",
+      },
+      {
+        internalType: "bytes32",
+        name: "salt",
+        type: "bytes32",
+      },
+      {
+        internalType: "uint256[]",
+        name: "extensions",
+        type: "uint256[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "forwarder",
+        type: "address",
+      },
+    ],
+    name: "isTrustedForwarder",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "mint",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "name",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+    ],
+    name: "nonces",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "deadline",
+        type: "uint256",
+      },
+      {
+        internalType: "uint8",
+        name: "v",
+        type: "uint8",
+      },
+      {
+        internalType: "bytes32",
+        name: "r",
+        type: "bytes32",
+      },
+      {
+        internalType: "bytes32",
+        name: "s",
+        type: "bytes32",
+      },
+    ],
+    name: "permit",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "renounceOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "symbol",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "totalSupply",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "transfer",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "transferFrom",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "transferOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];

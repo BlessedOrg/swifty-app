54b9aaa5242f20970890d2ef5ed8e25a16254187
adiusz <leeszczuu@gmail.com>
2024-05-24 16:08:54 +0200

    LV2: add slider for tolerance instead of input

--- src/components/createEvent/createEventForm/phasesSettings/PhasesSettings.tsx
+++ src/components/createEvent/createEventForm/phasesSettings/PhasesSettings.tsx
@@ -1,14 +1,7 @@
-import {
-  FormErrorMessage,
-  Tab,
-  TabList,
-  TabPanel,
-  TabPanels,
-  Tabs,
-} from "@chakra-ui/react";
+import { FormErrorMessage, Slider, SliderFilledTrack, SliderMark, SliderThumb, SliderTrack, Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
 import { LineChart, Ticket, Timer } from "lucide-react";
 import { FormField, FormInput } from "../FormFields";
-import {Controller} from "react-hook-form";
+import { Controller } from "react-hook-form";
 
 const tabs = [
   { id: "lotteryV1settings", title: "Lottery 1" },
@@ -16,6 +9,12 @@ const tabs = [
   { id: "auctionV1settings", title: "Auction 1" },
   { id: "auctionV2settings", title: "Auction 2" },
 ];
+
+const labelStyles = {
+  mt: '2',
+  ml: '-2.5',
+  fontSize: 'sm',
+}
 
 export const PhasesSettings = ({ register, errors, control }) => {
   return (
@@ -118,26 +117,39 @@ export const PhasesSettings = ({ register, errors, control }) => {
               )}
               {tab.id === "lotteryV2settings" && (
                 <FormField
-                  label={"Tolerance (1-99%)"}
+                  label={"Tolerance"}
                   isInvalid={!!errors?.[tab.id]?.rollTolerance}
-                  errorMessage={
-                    <FormErrorMessage>{`${errors?.[tab.id]?.rollTolerance
-                      ?.message}`}</FormErrorMessage>
-                  }
+                  errorMessage={<FormErrorMessage>{`${errors?.[tab.id]?.rollTolerance?.message}`}</FormErrorMessage>}
                 >
                   <Controller
+                    defaultValue={50}
                     render={({ field }) => (
-                      <FormInput
-                        type={"number"}
-                        icon={LineChart}
-                        id={`${tab.id}.rollTolerance`}
-                        placeholder={"Tolerance in range 1-99%"}
-                        onChange={(e) => {
-                          const v = +e.target.value;
-                          field.onChange(v);
-                        }}
-                        value={field.value}
-                      />
+                      <Slider min={1} max={99} defaultValue={50} aria-label='slider-ex-6' onChange={(v) => field.onChange(v)} mt={6}>
+                        <SliderMark value={1} {...labelStyles}>
+                          1%
+                        </SliderMark>
+                        <SliderMark value={50} {...labelStyles}>
+                          50%
+                        </SliderMark>
+                        <SliderMark value={99} {...labelStyles}>
+                          99%
+                        </SliderMark>
+                        <SliderMark
+                          value={field.value}
+                          textAlign="center"
+                          color="black"
+                          mt="-10"
+                          ml="-5"
+                          w="12"
+                          bg={"var(--neonGreen)"}
+                        >
+                          {field.value}%
+                        </SliderMark>
+                        <SliderTrack>
+                          <SliderFilledTrack bg={"var(--neonGreen)"} />
+                        </SliderTrack>
+                        <SliderThumb />
+                      </Slider>
                     )}
                     name={`${tab.id}.rollTolerance`}
                     control={control}

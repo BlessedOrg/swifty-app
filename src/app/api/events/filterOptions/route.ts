d8397249ad9a34f2c5428b2c54b924e8f1b36da2
Filip <82238474+jumperdevweb3@users.noreply.github.com>
2024-05-31 09:03:59 +0200

    fix: filter caching

--- src/app/api/events/filterOptions/route.ts
+++ src/app/api/events/filterOptions/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import { eventLocation, speaker } from "@/prisma/models";
 import { revalidatePath } from "next/cache";
 
+export const dynamic = 'force-dynamic'
+
 export async function GET(req: Request) {
   try {
     const getEventsByContinent = async () => {

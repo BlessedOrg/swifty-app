a05a45e3449632ee73fd0c4044974b338ec8c2b3
Filip <82238474+jumperdevweb3@users.noreply.github.com>
2024-07-01 18:20:30 +0200

    fix: acc create

--- src/utils/thirdweb/checkIfAccountIsAbstracted.ts
+++ src/utils/thirdweb/checkIfAccountIsAbstracted.ts
@@ -1,26 +1,32 @@
 import { client } from "lib/client";
-import { getContract, readContract} from "thirdweb"
-import { selectedNetwork } from "../../services/viem";
-export const checkIfAccountIsAbstracted = async (address: string) => {
-  const contractABI: any[] = [
-    {
-      "inputs":[],
-      "name":"getAllAccounts",
-      "outputs":[{"internalType":"address[]","name":"","type":"address[]"}],
-      "stateMutability":"view",
-      "type":"function"
-    },
-  ];
-  const contract = getContract({
-    client: client,
-    chain: selectedNetwork,
-    abi: contractABI,
-    address: process.env.THIRDWEB_FACTORY_ADDRESS as string,
-  });
-  const abstractedAccounts = await readContract({
-    contract,
-    method: "getAllAccounts"
-  })
+import { getContract, readContract } from "thirdweb";
+import { selectedNetwork } from "services/viem";
 
-  return !!abstractedAccounts.includes(address);
+export const checkIfAccountIsAbstracted = async (address: string) => {
+  try {
+    const contractABI: any[] = [
+      {
+        "inputs": [],
+        "name": "getAllAccounts",
+        "outputs": [{ "internalType": "address[]", "name": "", "type": "address[]" }],
+        "stateMutability": "view",
+        "type": "function",
+      },
+    ];
+    const contract = getContract({
+      client: client,
+      chain: selectedNetwork,
+      abi: contractABI,
+      address: process.env.THIRDWEB_FACTORY_ADDRESS as string,
+    });
+    const abstractedAccounts = await readContract({
+      contract,
+      method: "getAllAccounts",
+    });
+
+    return abstractedAccounts?.includes(address);
+  } catch (error: any) {
+    console.log("🚨 Error while checking if Account is Abstracted: ", error.message);
+    return false;
+  }
 };
